aae12c17ebd9c72c3eec7644930aab7e0995c630
manoj gokina <manojgokina@gmail.com>
2022-02-05 12:54:30 +0530

    UI changes.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 import {Provider} from 'react-redux';
 import store from './app/store';
 
